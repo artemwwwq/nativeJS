79d832d5e8d3d8c1fc102a9e1231ccae4c708f65
artemwwwq <artem201190@mail.ru>
2023-01-18 18:08:02 +0500

    JS TESTS SECOND WORK

---
+++ src/01-hello-tests/02/02.ts
@@ -0,0 +1,53 @@
+
+type LocalCityType = {
+    title: string
+    countryTitle: string
+}
+
+type TechType = {
+    id: number
+    title: string
+}
+
+
+type AddressType = {
+    streetTitle: string
+    city: LocalCityType
+
+}
+
+
+type StudentType = {
+    id: number
+    name: string
+    age: number
+    isActive: boolean
+    address: AddressType
+    technologies: Array<TechType>
+}
+
+const students: StudentType = {
+    id: 1,
+    name: 'Artem',
+    age: 22,
+    isActive: true,
+    address: {
+        streetTitle: 'Transportnikov 50A',
+        city: {
+            title: 'Yekaterinburg',
+            countryTitle: 'Russia'
+        }
+    },
+    technologies: [
+        {
+            id: 1,
+            title: 'HTML'
+        }, {
+            id: 2,
+            title: 'CSS'
+        }, {
+            id: 3,
+            title: 'JS'
+        }
+    ]
+}
